19423bdb4d66a37b7474f9d8c3db2d65bd5ffd6b
Lorena Rangel <lorena.rangel@docker.com>
2020-11-01 22:24:53 +0100

    add bootstrap styles
    Signed-off-by: Lorena Rangel <lorena.rangel@docker.com>

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <html>
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title> Tienda de libros online </title>
 </head>
 
 <body>
-    <h1>Bienvenidos a mi tienda</h1>
-    <h2>Listado de libros disponibles: </h2>
+    <div class="jumbotron text-center">
+        <h1>Bienvenidos a mi tienda</h1>
+        <h2>Listado de libros disponibles: </h2>
+    </div>
 
     <?php
     $availableBooks = array(
@@ -15,10 +20,15 @@
         'discrete and combinatorial mathematics' => -1,
         'the mythical man month' => 2,
         'do androids dream of electric sheep?' => 0,
+        'inspired' => 7,
+        'code' => 1,
+        'neuromancer' => 0,
+        'pattern classification' => 5,
+        '1984' => 10,
     );
 
-    echo '<ul>';
-    foreach ($availableBooks as $book => $available) {
+    echo '<ul class="list-group">';
+    foreach ($availableBooks as $book => $count) {
         prettyPrint($book, $count);
     }
     echo '</ul>';
@@ -31,7 +41,7 @@
         } else {
             $color = 'green';
         }
-        echo '<li style="color:' . $color . ';">' . $prettyName . ', ' . $count . '</li>';
+        echo '<li class="list-group-item" style="color:' . $color . ';">' . $prettyName . ', ' . $count . '</li>';
     }
 
     ?>
